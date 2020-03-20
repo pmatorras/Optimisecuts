dd0c2006617c7d91df3721c73b52144720bce8b3
pmatorras <pablo.matorras@cern.ch>
2020-03-20 20:35:40 +0100

    cleaned up program and display

--- drawhistos.py
+++ drawhistos.py
@@ -1,19 +1,62 @@
 from ROOT import TCanvas, TPad, TFile, TPaveLabel, TPaveText, TLegend
 from ROOT import gROOT,gDirectory, TTree
-from collections import OrderedDict
-import os
+import os, optparse
+
 optim="/home/pablinux/cernbox/www/susy/optimisation"
 hfolder='../Histograms/'
 hfilenm="../Output/allpostcuts.root"
 hfile  = TFile(hfilenm,"READ","Example");
 os.system("mkdir -p "+hfolder)
 
-gROOT.SetBatch(1)
+#set x and y ranges
+def setRanges(hT2tt,httbar):
+    xMin=-999
+    xMax= 999
+    yMax= 999
+    yMin= 0
+    yMax=1.1*max(hT2tt.GetMaximum(),httbar.GetMaximum())
+    xMin=min(hT2tt.GetXaxis().GetXmin(),httbar.GetXaxis().GetXmin())
+    xMax= max(hT2tt.GetXaxis().GetXmax(),httbar.GetXaxis().GetXmax())
+    yMin=0.9*min(hT2tt.GetMinimum(),httbar.GetMinimum())
+    hT2tt.GetYaxis().SetRangeUser(yMin,yMax)
+    hT2tt.GetXaxis().SetRangeUser(xMin,xMax)
+
+#Scale histograms
+def Scalehisto(histo,varControl):
+    norm=histo.GetSumOfWeights()
+    if(norm<0.0001): norm=1.0
+    if varControl is False: histo.Scale(1/norm)
+
+#check if histogram exists
+def histoexists(histo, hnm):
+    cont=False
+    histo_type=str(type(histo)) #'ROOT.TH1F'>"
+    if (bool('ROOT.TH1F' not in histo_type)):
+        print "-----------------------------------"
+        print "|  "+hnm+" not a histogram\t  |"
+        print "| perhaps variable doesn't exist? |"
+        print "-----------------------------------"
+        cont=True
+    return cont
+            
+
+#Optional parameters
+usage = 'usage: %prog [options]'
+parser = optparse.OptionParser(usage)
+parser.add_option('--test' , dest='test' , help='run only few plits', default=False, action='store_true')
+parser.add_option('--rmold' , dest='rmold' , help='remove old images', default=False, action='store_true')
+parser.add_option('--nobatch' , dest='nobatch' , help='remove old images', default=False, action='store_true')
+(opt, args) = parser.parse_args()
+
+#Apply options
+if opt.rmold is True:
+    print "removing histograms in", hfolder
+    os.system('rm -r '+hfolder)
+gROOT.SetBatch(not(opt.nobatch))
 c1 = TCanvas( 'c1', 'Dynamic Filling Example', 200,\
  10, 1600, 900 )
-doLogy=False
-doTest=True
-#doTest=False
+
+#Define variables to over which make histograms
 variables=["evid_",  "nSV_","Dnjetstot_", "isSF_", \
            "btagW_", "bvetoW_","MET_sumEt_","MET_pt_", \
            "PV_x_", "PV_y_", "PV_z_", "PV_npvs_", "PV_chi2_",\
@@ -22,119 +65,92 @@ variables=["evid_",  "nSV_","Dnjetstot_", "isSF_", \
            "mt2ll_", "nLepton_", "ptmiss_", "susyMstop_","susyMLSP__",\
            "lep1_pt_","lep2_pt_","jet1_pt_","jet2_pt_","dphill_",\
            "detall_","dRll_","dphijj_","detajj_","dRjj_"]
-controlvars=["evid_","btagW_","bvetoW_", "susyMstop_","susyMLSP__"]
+controlvars=["evid_","btagW_","bvetoW_", "susyMstop_","susyMLSP__","is_SF_"]
 flavours={'df':'-1','sf':'1'}
 bjets={'btag': 'btagW_','bveto':'bvetoW_'}
-samples=OrderedDict({'T2tt':'hT2tt', 'ttbar':'httbar'})
+samples={'T2tt':'hT2tt', 'ttbar':'httbar'}
 mStops={'mS-400to700' :'&& susyMstop>=400 && susyMstop<600',\
         'mS-700to1200':'&& susyMstop>=700 && susyMstop<=1200'       }
 
 print "Creating histograms:"
 for var in variables:
-    if doTest==True and "MET_pt" not in var: continue
+    #Save in different folder if control variable
+    cfolder=''
+    isControl=bool(var in controlvars)        
+    if(isControl is True): cfolder='Control/'
+
+    if opt.test==True and "MET_pt" not in var: continue
     btagVeto=False
     if(var in bjets.values()): btagVeto=True 
     for bjet in bjets:
+        
         if btagVeto is True and bjet not in var:
             print "Omitting histogram since var="+var+" and bjet="+bjet
             continue
         os.system('mkdir -p '+hfolder+bjet)
         for fl in flavours:
-            normT2tt=1
-            normttbar=1
-            nttbar=1
+            histnm = var+bjet+'_'+fl
             for sample in samples:
-                print "sample",sample
-                tree=hfile.Get(sample)
-                hterm='_'+var+'_'+bjet+'_'+fl
-                histo= samples[sample]+hterm
-                
-                treevar=var+sample+">>"+histo
-                flcond="isSF_"+sample+"=="+flavours[fl]
-                onlyfill=var+sample+'>-999'
-                bjetcond=bjets[bjet]+sample
-                condition=bjetcond+'*('+flcond+'&&'+onlyfill+')'
+                #make histo for each variable and condition
+                tree      = hfile.Get(sample)
+                histo     = samples[sample]+histnm
+                treevar   = var+sample+">>"+histo
+                flcond    = "isSF_"+sample+"=="+flavours[fl]
+                onlyfill  = var+sample+'>-999'
+                bjetcond  = bjets[bjet]+sample
+                condition = bjetcond+'*('+flcond+'&&'+onlyfill+')'
                 tree.Draw(treevar, condition)
-                              
+
                 #divide in different mass ranges
-                histomStop = histo
                 for mstop in mStops:
                     if sample not in 'T2tt': continue
-                    #print "sample",sample, mstop
-                    splitcond=condition.split(')')
-                    mStopcond=splitcond[0]+mStops[mstop]+')'
-                    histomStop=histo+'_'+mstop
-                    treevarmStop=var+sample+'>>'+histomStop
-                    #print histomStop
-                    tree.Draw(treevarmStop,mStopcond)
-                
-
-            histosig= samples['T2tt']+hterm
-            histnm = var+bjet+'_'+fl
+                    splitcond    = condition.split(')')
+                    mStopcond    = splitcond[0]+mStops[mstop]+')'
+                    treevarmStop = var+sample+'>>'+histo+'_'+mstop
+                    tree.Draw(treevarmStop,mStopcond)                
+            
+            histosig= samples['T2tt']+histnm
             print "HISTOGRAM:\t", histnm
 
-            #Get and normalise histograms
-            hT2tt = gDirectory.Get(samples["T2tt"]+hterm);
-            hT2tt_type=str(type(hT2tt)) #'ROOT.TH1F'>"
-            if (bool('ROOT.TH1F' not in hT2tt_type)):
-                print "----------------------------------"
-                print "| hT2tt not a histogram          |"
-                print "| perhaps variable doesn't exist?|"
-                print "----------------------------------"
-                continue
-            httbar = gDirectory.Get(samples["ttbar"]+hterm)
-            normT2tt  = hT2tt.GetSumOfWeights();
-            normttbar = httbar.GetSumOfWeights();
-            nttbar=httbar.GetEntries()
-            if normttbar<0.01: normttbar=1.0
-            bjetnm=bjet
-            if(var in controlvars):
-                bjetnm+='/Control'
-            else:
-                hT2tt.Scale(1/normT2tt);
-                httbar.Scale(1/normttbar);
+            #Get  histograms and check its existance
+            hT2tt      = gDirectory.Get(samples["T2tt"]+histnm);
+            httbar     = gDirectory.Get(samples["ttbar"]+histnm)
+            cont_T2tt  = histoexists(hT2tt,"hT2tt")
+            cont_ttbar = histoexists(httbar,"httbar")
+            if(True in [cont_T2tt,cont_ttbar]): continue
             
-            #set x and y ranges
-            xMin=-999
-            xMax=-999
-            yMax= 999
-            yMin= 0
-            yMax=max(hT2tt.GetMaximum(),httbar.GetMaximum())
-            xMin=min(hT2tt.GetXaxis().GetXmin(),httbar.GetXaxis().GetXmin())
-            xMax= max(hT2tt.GetXaxis().GetXmax(),httbar.GetXaxis().GetXmax())
-            yMin=min(hT2tt.GetMinimum(),httbar.GetMinimum())
-            hT2tt.GetYaxis().SetRangeUser(0.9*yMin,1.1*yMax)
-            hT2tt.GetXaxis().SetRangeUser(xMin,xMax)
+            #Set proper ranges
+            Scalehisto(httbar,isControl)
+            Scalehisto(hT2tt,isControl)
+            setRanges(hT2tt,httbar)
 
-            #Set title and legend
+            #Draw histogram and legend
             hT2tt.SetTitle(histnm)
             hT2tt.SetStats(False)
-            hT2tt.SetLineColor(2)
+            hT2tt.SetLineColor(2)            
+            hT2tt.Draw('hist')
+            httbar.Draw("hist same")
+
             legend = TLegend(0.7,0.7,0.9,0.9);
             legend.AddEntry(hT2tt,"T2tt","f");
             legend.AddEntry(httbar,"ttbar","f");
 
-            
-            hT2tt.Draw('hist')
-            httbar.Draw("hist same")
-            
+            #draw masspoint ranges
             for idx,mstop in enumerate(mStops):
-                histomStop=histosig+'_'+mstop
-                print "eeee",histomStop
-                hmStop = gDirectory.Get(histomStop)
+                hmStop = gDirectory.Get(histosig+'_'+mstop)
                 normmStop  = hmStop.GetSumOfWeights();
                 hmStop.Scale(1/normmStop);
                 hmStop.SetLineColor(idx+6)
-                hmStop.Draw('hist same')
+                hmStop.Draw('same')
                 legend.AddEntry(hmStop,"T2tt-"+mstop,"f");
-
 
             legend.Draw()
 
-            outputfolder=hfolder+bjetnm+'/'
+            #save to image
+            outputfolder=hfolder+bjet+'/'+cfolder
             os.system('mkdir -p '+outputfolder)
             os.system("cp "+optim+'/index.php '+outputfolder)
-            #c1.SaveAs(outputfolder+histnm+'.png')
+            c1.SaveAs(outputfolder+histnm+'.png')
 
 print "\nFinished drawing histograms.\nCopying to "+optim
 os.system("mkdir -p "+optim )
