88790e8e4554ab3ebd1da251ef8c963f24008428
pmatorras <pablo.matorras@cern.ch>
2020-03-20 12:51:19 +0100

    added the new variables to the plotting script

--- drawhistos.py
+++ drawhistos.py
@@ -1,6 +1,5 @@
 from ROOT import TCanvas, TPad, TFile, TPaveLabel, TPaveText, TLegend
 from ROOT import gROOT,gDirectory, TTree
-from definebranches import hfilenm, defVectors
 import os
 
 hfolder='../Histograms/'
@@ -19,7 +18,9 @@ variables=["evid_",  "nSV_","Dnjetstot_", "isSF_", \
            "PV_x_", "PV_y_", "PV_z_", "PV_npvs_", "PV_chi2_",\
            "SV_eta_", "SV_phi_", "SV_pt_", "SV_mass_", \
            "SV_x_", "SV_y_", "SV_z_", "SV_chi2_","mll_", \
-           "mt2ll_", "nLepton_", "ptmiss_", "susyMstop_", "susyMLSP__"]
+           "mt2ll_", "nLepton_", "ptmiss_", "susyMstop_","susyMLSP__",\
+           "lep1_pt_","lep2_pt_","jet1_pt_","jet2_pt_","dphill_",\
+           "detall_","dRll_","dphijj_","detajj_","dRjj_"]
   
 flavours={'df':'-1','sf':'1'}
 bjets={'btag': 'btagW_','bveto':'bvetoW_'}
@@ -38,9 +39,11 @@ for var in variables:
                 hterm='_'+var+'_'+bjet+'_'+fl
                 histo=var+sample+">>"+samples[sample]+hterm
                 flcond="isSF_"+sample+"=="+flavours[fl]
+                onlyfill=var+sample+'>-999'
                 bjetcond=bjets[bjet]+sample
-                condition=bjetcond+'*('+flcond+')'
+                condition=bjetcond+'*('+flcond+'&&'+onlyfill+')'
                 #print "condition", condition, "histo", histo
+
                 tree.Draw(histo, condition)
 
             histnm = var+bjet+'_'+fl
@@ -48,8 +51,13 @@ for var in variables:
 
             #Get and normalise histograms
             hT2tt = gDirectory.Get(samples["T2tt"]+hterm);
-            hT2tt.SetTitle(histnm)
-            hT2tt.SetStats(False)
+            hT2tt_type=str(type(hT2tt)) #'ROOT.TH1F'>"
+            if (bool('ROOT.TH1F' not in hT2tt_type)):
+                print "----------------------------------"
+                print "| hT2tt not a histogram          |"
+                print "| perhaps variable doesn't exist?|"
+                print "----------------------------------"
+                continue
             httbar = gDirectory.Get(samples["ttbar"]+hterm)
             normT2tt  = hT2tt.GetSumOfWeights();
             normttbar = httbar.GetSumOfWeights();
@@ -58,24 +66,25 @@ for var in variables:
             hT2tt.Scale(1/normT2tt);
             httbar.Scale(1/normttbar);
             
-            #Make legend
-            legend = TLegend(0.7,0.7,0.9,0.9);
-            legend.AddEntry(hT2tt,"T2tt","f");
-            legend.AddEntry(httbar,"ttbar","f");
-
             #set x and y ranges
             xMin=-999
             xMax=-999
             yMax= 999
             yMin= 0
-
             yMax=max(hT2tt.GetMaximum(),httbar.GetMaximum())
             xMin=min(hT2tt.GetXaxis().GetXmin(),httbar.GetXaxis().GetXmin())
             xMax= max(hT2tt.GetXaxis().GetXmax(),httbar.GetXaxis().GetXmax())
             yMin=min(hT2tt.GetMinimum(),httbar.GetMinimum())
             hT2tt.GetYaxis().SetRangeUser(0.9*yMin,1.1*yMax)
             hT2tt.GetXaxis().SetRangeUser(xMin,xMax)
+
+            #Set title and legend
+            hT2tt.SetTitle(histnm)
+            hT2tt.SetStats(False)
             hT2tt.SetLineColor(2)
+            legend = TLegend(0.7,0.7,0.9,0.9);
+            legend.AddEntry(hT2tt,"T2tt","f");
+            legend.AddEntry(httbar,"ttbar","f");
 
             
             hT2tt.Draw('hist')
