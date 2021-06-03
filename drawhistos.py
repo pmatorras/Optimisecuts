8752fd672fb9dbe718769cd50cb0a8c4212e87e1
Pablo <pablo.matorras@cern.ch>
2021-06-03 10:27:43 +0200

    cleanup drawhistos

--- drawhistos.py
+++ drawhistos.py
@@ -2,24 +2,24 @@ from ROOT import TCanvas, TPad, TFile, TPaveLabel, TPaveText, TLegend
 from ROOT import gROOT,gDirectory, TTree
 from collections import OrderedDict
 import os, optparse
-wloc=os.environ['WWW']
-optim=wloc+"/susy/optimisation"
-hfolder='Histograms/'
-hfilenm="Output/allcuts.root"
-hfile  = TFile(hfilenm,"READ","Example");
+wloc    = os.environ['WWW']
+optim   = wloc+"/susy/optimisation"
+hfolder = 'Histograms/'
+hfilenm = "Output/allcuts.root"
+hfile   = TFile(hfilenm,"READ","Example");
 os.system("mkdir -p "+hfolder)
 
 #set x and y ranges
 def setRanges(hT2tt,httbar):
-    xMin=-999
-    xMax= 999
-    yMax= 999
-    yMin= 0
+    xMin = -999
+    xMax =  999
+    yMax =  999
+    yMin =  0
     #    histo=
-    yMax=1.1*max(hT2tt.GetMaximum(),httbar.GetMaximum())
-    xMin=min(hT2tt.GetXaxis().GetXmin(),httbar.GetXaxis().GetXmin())
-    xMax= max(hT2tt.GetXaxis().GetXmax(),httbar.GetXaxis().GetXmax())
-    yMin=0.9*min(hT2tt.GetMinimum(),httbar.GetMinimum())
+    yMax = 1.1*max(hT2tt.GetMaximum(),httbar.GetMaximum())
+    xMin = min(hT2tt.GetXaxis().GetXmin(),httbar.GetXaxis().GetXmin())
+    xMax = max(hT2tt.GetXaxis().GetXmax(),httbar.GetXaxis().GetXmax())
+    yMin = 0.9*min(hT2tt.GetMinimum(),httbar.GetMinimum())
     hT2tt.GetYaxis().SetRangeUser(yMin,0.4)#yMax)
     #hT2tt.GetXaxis().SetRangeUser(xMin,xMax)
     #print xMax, xMin, "-----------------------------"
@@ -27,59 +27,58 @@ def setRanges(hT2tt,httbar):
 def ScaleToMax(histos,varControl):
     for histo in histos:
         if varControl is True: continue
-        norm=1
-        maxBin=histo.GetMaximumBin()
-        norm=histo.GetBinContent(maxBin)
+        norm   = 1
+        maxBin = histo.GetMaximumBin()
+        norm   = histo.GetBinContent(maxBin)
         histo.Scale(1/norm)
 
 #Scale histograms
 def ScaleToInt(histos,varControl):
     for sample in histos:
-        histo=gDirectory.Get(histos[sample])
-        #histo=histos[sample]
-        norm=histo.GetSumOfWeights()
-        if(norm<0.0001): norm=1.0
+        histo = gDirectory.Get(histos[sample])
+        norm  = histo.GetSumOfWeights()
+        if(norm<0.0001): norm = 1.0
         if varControl is False: histo.Scale(1/norm)
         if norm<10: histo.Scale(0.1)
         #print "NORM", histo.GetName(), histo.GetEntries()
 #check if histogram exists
 def OLDhistoexists(histo, hnm):
-    cont=False
-    histo_type=str(type(histo)) #'ROOT.TH1F'>"
+    cont       = False
+    histo_type = str(type(histo)) #'ROOT.TH1F'>"
     if (bool('ROOT.TH1F' not in histo_type)):
         print "-----------------------------------"
         print "|  "+hnm+" not a histogram\t  |"
         print "| perhaps variable doesn't exist? |"
         print "-----------------------------------"
-        cont=True
+        cont = True
     return cont
 #check if histogram exists
 def histoexists(histo, hnm):
-    cont=False
-    histo_type=str(type(histo)) #'ROOT.TH1F'>"
+    cont       = False
+    histo_type = str(type(histo)) #'ROOT.TH1F'>"
     if (bool('ROOT.TH1F' not in histo_type)):
         print "-----------------------------------"
         print "|  "+hnm+" not a histogram\t  |"
         print "| perhaps variable doesn't exist? |"
         print "-----------------------------------"
-        cont=True
+        cont = True
     return cont
 
 #Scale x axis
 def findXrange(hdict, binWOri,nBinFin,xMinOri):
-    xMinBin=999999
-    xMaxBin=0
+    xMinBin = 999999
+    xMaxBin = 0
     for samp in hdict:
-        histo=gDirectory.Get(hdict[samp])
-        xMinBin=min(xMinBin,histo.FindFirstBinAbove(0.1))
-        xMaxBin=max(xMaxBin,histo.FindLastBinAbove(0.1))
+        histo   = gDirectory.Get(hdict[samp])
+        xMinBin = min(xMinBin,histo.FindFirstBinAbove(0.1))
+        xMaxBin = max(xMaxBin,histo.FindLastBinAbove(0.1))
     if(xMinBin>xMaxBin):
-        xMinBin=0
-        xMaxBin=nBinFin
-    nBin=xMaxBin-xMinBin
-    xLen=nBin*binWOri
-    widthNew=xLen/nBinFin
-    scaleFactor=int(widthNew/binWOri)
+        xMinBin = 0
+        xMaxBin = nBinFin
+    nBin        = xMaxBin-xMinBin
+    xLen        = nBin*binWOri
+    widthNew    = xLen/nBinFin
+    scaleFactor = int(widthNew/binWOri)
     while(scaleFactor>1 and nBinOri%scaleFactor>0): scaleFactor-=1
     if(scaleFactor<1): scaleFactor=1
     xMax=xMinOri+xMaxBin*binWOri
@@ -93,10 +92,10 @@ def findXrange(hdict, binWOri,nBinFin,xMinOri):
 def findYrange(hdict, scaleFactor):
     yMax=0
     for samp in hdict:
-        histo=gDirectory.Get(hdict[samp])                
+        histo   = gDirectory.Get(hdict[samp])                
         histo.Rebin(scaleFactor)
-        ymaxBin=histo.GetMaximumBin()
-        yMax=max(yMax,histo.GetBinContent(ymaxBin))
+        ymaxBin = histo.GetMaximumBin()
+        yMax    = max(yMax,histo.GetBinContent(ymaxBin))
     return yMax
             
 
@@ -156,18 +155,19 @@ ISRcut={"ISR": "ISRcut>0", "Inclusive":"1==1", "lowMT2": "mt2ll<20", "lowMT2_ISR
 print "Creating histograms:"
 c1 = TCanvas( 'c1', 'Dynamic Filling Example', 200,10, 1600, 900 )
 if  opt.test is True:
-    variables=testvars
+    variables = testvars
 else:
-    variables=allvars
+    variables = allvars
 for var in variables:
     #Save in different folder if control variable
-    cfolder=''
-    isControl=bool(var in controlvars)        
-    if(isControl is True): cfolder='Control/'
+    cfolder   = ''
+    isControl = bool(var in controlvars)        
+    if(isControl is True): cfolder = 'Control/'
 
     if opt.var is not False and opt.var not in var: continue
-    btagVeto=False
-    if(var in bjets.values()): btagVeto=True 
+    btagVeto = False
+    if(var in bjets.values()): btagVeto = True 
+    print "VARIABLE", var
     for bjet in bjets:
         if allsamples is False and opt.btag  is True and "btag"  not in bjet: continue  
         if allsamples is False and opt.bveto is True and "bveto" not in bjet: continue
@@ -180,55 +180,54 @@ for var in variables:
             if allsamples is False and opt.lowMT2 is False and "lowMT"     in isr: continue
             if allsamples is False and opt.lowMT2 is True  and "lowMT" not in isr: continue
 
-            samplefolder=hfolder+bjet+'/'+isr
-            isrnm=isr
-            if isrnm in "Inclusive": isrnm="All"
+            samplefolder = hfolder+bjet+'/'+isr
+            isrnm        = isr
+            if isrnm in "Inclusive": isrnm = "All"
             os.system('mkdir -p '+samplefolder)
             for fl in flavours:
-                varsplit=var.split(':')
-                varnm=''
-                ptcond1=""#smalldphi"#"smalldphi"
-                histnm = varnm+'_'+bjet+'_'+fl+'_'+isrnm
-                nBinOri= 500000
-                nBinFin=     12
-                xMinOri=      0.
-                xMaxOri=   5000.
+                varsplit = var.split(':')
+                varnm    = var
+                ptcond1  = ""#smalldphi"#"smalldphi"
+                histnm   = varnm+'_'+bjet+'_'+fl+'_'+isrnm
+                nBinOri  = 500000
+                nBinFin  =     12
+                xMinOri  =      0.
+                xMaxOri  =   5000.
                 if "evid" in var:
-                    xminOri=0
-                    xMaxOri=1000000
-                    nBinOri=1000000
+                    xminOri = 0
+                    xMaxOri = 1000000
+                    nBinOri = 1000000
                 if "SV" in var:
-                    xMinOri=    -5
-                    xMaxOri=  1000.
-                    nBinOri=100000
+                    xMinOri =     -5
+                    xMaxOri =   1000.
+                    nBinOri = 100000
                 if "deta" in var or "dphi" in var or "dR" in var:
-                    xMinOri=   0.
-                    xMaxOri=   5.
-                    nBinOri=5000
+                    xMinOri =    0.
+                    xMaxOri =    5.
+                    nBinOri = 5000
                 if "SV_chi2" in var or "SV_x" in var or "SV_y" in var or "SV_z" in var:
-                    xMinOri=-20.
-                    xMaxOri= 20.
-                    nBinOri=10000
+                    xMinOri =  -20.
+                    xMaxOri =   20.
+                    nBinOri = 10000
 
                 if var in "PV_y":
-                    xMinOri= 0.15
-                    xMaxOri= 0.20
-                    nBinOri=10000
+                    xMinOri =  0.15
+                    xMaxOri =  0.20
+                    nBinOri = 10000
 
                 if var in "PV_x":
-                    xMinOri= 0.09
-                    xMaxOri= 0.12
-                    nBinOri=10000
+                    xMinOri =  0.09
+                    xMaxOri =  0.12
+                    nBinOri = 10000
                 if(xMaxOri-xMinOri<0.000001): xMaxOri=xMinOri+1
-                lenOri= (xMaxOri-xMinOri)
-                #print "check", lenOri, nBinOri
-                binWOri=lenOri/nBinOri
-                xRanOri=""
-                is2D=bool(len(var.split(':'))>1)
+                lenOri  = (xMaxOri-xMinOri)
+                binWOri = lenOri/nBinOri
+                xRanOri = ""
+                is2D    = bool(len(var.split(':'))>1)
                 if(is2D is False):
-                    xRanOri= "("+str(nBinOri)+","+str(xMinOri)+","+str(xMaxOri)+")"
+                    xRanOri = "("+str(nBinOri)+","+str(xMinOri)+","+str(xMaxOri)+")"
                 elif is2D is True and "ptlepsum"  in var :
-                    xRanOri= "(30,40,300,30,0,3.15)"
+                    xRanOri = "(30,40,300,30,0,3.15)"
                 dtest=OrderedDict({})
                 for sample in samples:
                     #make histo for each variable and condition
@@ -238,7 +237,7 @@ for var in variables:
                     histo     = samples[sample]+histnm
                     
                     onlyfill = ' && '
-                    number ='-999'
+                    number   = '-999'
                     if is2D is False:
                         if "dphi" in var: number='-1'
                         onlyfill += var+">"+number+ " && "
@@ -267,53 +266,50 @@ for var in variables:
             
                 #Set proper ranges and rebin histogram
                 if(is2D is False):
-                    xMin, xMax, scaleFactor= findXrange(dtest, binWOri, nBinFin, xMinOri)
+                    xMin, xMax, scaleFactor = findXrange(dtest, binWOri, nBinFin, xMinOri)
                     ScaleToInt(dtest, isControl)
-                    yMax=findYrange(dtest, scaleFactor)
+                    yMax   = findYrange(dtest, scaleFactor)
                     legend = TLegend(0.7,0.7,0.9,0.9);
                     for idx,sam in enumerate(dtest):
-                        isSame='same'
-                        #print "dtest", dtest
-                        hist=gDirectory.Get(dtest[sam])
+                        isSame = 'same'
+                        hist   = gDirectory.Get(dtest[sam])
                         if idx is 0:
                             hist.GetXaxis().SetRangeUser(xMin,xMax)
                             hist.GetYaxis().SetRangeUser(0,1.1*yMax)
                             hist.SetTitle(histnm)
                             hist.SetStats(False)
 
-                            isSame=''
+                            isSame = ''
                             hist.Draw('hist')
                         if sam in "T2tt":
                             hist.Draw('hist same')
                             hist.SetLineColor(2)
-                            #print "histo", hist, xMin, xMax
                         for ms in mStops:
                             if ms in sam :
-                                #print "colors", type( hist), hist.GetName()
                                 hist.SetLineColor(mStops[ms][1])
                                 hist.Draw('same')
-                        isBkg=''
-                        lab="le"
+                        isBkg = ''
+                        lab   = "le"
                         if sam in bkgs:
-                            lab="f"
-                            isBkg='hist '
+                            lab   = "f"
+                            isBkg = 'hist '
                             hist.SetLineColor(bkgs[sam])
                         hist.Draw(isBkg+'same')
                         legend.AddEntry(hist,sam,lab);
                     legend.Draw()
                     #save to image
-                    outputfolder=samplefolder+'/'+cfolder
+                    outputfolder = samplefolder+'/'+cfolder
                     os.system('mkdir -p '+outputfolder)
                     os.system("cp "+optim+'/index.php '+outputfolder)
                     c1.SaveAs(outputfolder+histnm+'.png')
                 elif is2D is True:
             
-                    outputfolder=samplefolder+'/'+varnm+'/'+cfolder
+                    outputfolder = samplefolder+'/'+varnm+'/'+cfolder
                     os.system('mkdir -p '+outputfolder)
                     os.system("cp "+optim+'/index.php '+outputfolder)
                     
                     for sam in dtest:
-                        hist=gDirectory.Get(dtest[sam])
+                        hist = gDirectory.Get(dtest[sam])
                         hist.SetStats(False)
                         hist.SetTitle(histnm+'_'+sam)
                         #xMax=hist.FindLastBinAbove(1)
